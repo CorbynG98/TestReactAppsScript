42692894647842226eb0e739f76a9df5a645aa68
Corbyn Greenwood <corbyn.greenwood.98@gmail.com>
2024-12-03 20:49:15 -0500

    Trying browser router instead of base router

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -3,16 +3,10 @@ import { Col, Row } from 'react-bootstrap';
 import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
-import { useNavigate } from 'react-router';
+import { Link } from 'react-router';
 
 function NavBar() {
-    const navigate = useNavigate();
     const [isHovered, setIsHovered] = useState(false);
-    // Function to handle navigation based on query parameters
-    const handleNavigation = (page) => {
-        navigate(`?page=${page}`);
-    };
-
     return (
         <Navbar variant='dark' sticky='top' className='navSemiBackground' style={{ height: '6rem', padding: 0 }}>
             <Container style={{ paddingTop: '0.3rem', paddingBottom: '0.3rem' }}>
@@ -20,7 +14,8 @@ function NavBar() {
                     <Row style={{ display: 'flex', alignItems: 'center' }}>
                         <Col md='2'>
                             <Navbar.Brand
-                                onClick={() => handleNavigation('Home')}
+                                as={Link}
+                                to='/Home'
                                 onMouseEnter={() => setIsHovered(true)}
                                 onMouseLeave={() => setIsHovered(false)}
                                 style={{
@@ -43,10 +38,10 @@ function NavBar() {
                         <Col md='6'></Col>
                         <Col md='4'>
                             <div style={{ display: 'flex', columnGap: '1rem' }}>
-                                <Nav.Link style={{ color: 'white' }} onClick={() => handleNavigation('Test1')}>
+                                <Nav.Link style={{ color: 'white' }} as={Link} to='/Test1'>
                                     Test1
                                 </Nav.Link>
-                                <Nav.Link style={{ color: 'white' }} onClick={() => handleNavigation('Test2')}>
+                                <Nav.Link style={{ color: 'white' }} as={Link} to='/Test2'>
                                     Test2
                                 </Nav.Link>
                             </div>
--- src/pages/Renderer.tsx
+++ src/pages/Renderer.tsx
@@ -1,19 +1,20 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
-import HomePage from './HomePage';
+import { BrowserRouter } from 'react-router';
+import NavBar from '../components/NavBar';
+import RoutesComponent from '../components/RoutesComponent';
 
 export const Renderer = () => {
     return (
-        <HomePage />
-        // <Router>
-        //     <div
-        //         className='maxHeight'
-        //         style={{
-        //             backgroundColor: 'black',
-        //             background: 'radial-gradient(#262626, #000)',
-        //         }}>
-        //         <NavBar />
-        //         <RoutesComponent />
-        //     </div>
-        // </Router>
+        <BrowserRouter>
+            <div
+                className='maxHeight'
+                style={{
+                    backgroundColor: 'black',
+                    background: 'radial-gradient(#262626, #000)',
+                }}>
+                <NavBar />
+                <RoutesComponent />
+            </div>
+        </BrowserRouter>
     );
 };

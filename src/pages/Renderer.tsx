9255573e553438dbf241bd6cd08e3bbde34c8833
Corbyn Greenwood <corbyn.greenwood.98@gmail.com>
2024-12-03 20:39:51 -0500

    Seeing if simplifying it fixes the issues or something

--- src/pages/Renderer.tsx
+++ src/pages/Renderer.tsx
@@ -1,27 +1,19 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
-import { Helmet, HelmetProvider } from 'react-helmet-async';
-import { BrowserRouter as Router } from 'react-router';
-import NavBar from '../components/NavBar';
-import RoutesComponent from '../components/RoutesComponent';
+import HomePage from './HomePage';
 
 export const Renderer = () => {
     return (
-        <HelmetProvider>
-            <Helmet
-                titleTemplate='Apps Script Test | %s'
-                defaultTitle='Apps Script Test | Home'
-            />
-            <Router>
-                <div
-                    className='maxHeight'
-                    style={{
-                        backgroundColor: 'black',
-                        background: 'radial-gradient(#262626, #000)',
-                    }}>
-                    <NavBar />
-                    <RoutesComponent />
-                </div>
-            </Router>
-        </HelmetProvider>
+        <HomePage />
+        // <Router>
+        //     <div
+        //         className='maxHeight'
+        //         style={{
+        //             backgroundColor: 'black',
+        //             background: 'radial-gradient(#262626, #000)',
+        //         }}>
+        //         <NavBar />
+        //         <RoutesComponent />
+        //     </div>
+        // </Router>
     );
 };

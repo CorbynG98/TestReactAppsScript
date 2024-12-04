e5ac7697b911fab4a7e47e5fe49c9672529f426f
Corbyn Greenwood <corbyn.greenwood.98@gmail.com>
2024-12-03 20:23:48 -0500

    Tried reverted index.tsx back to what it was yesterday when working to see if that fixes my isseus

--- src/index.tsx
+++ src/index.tsx
@@ -1,9 +1,6 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import App from './App';
+import { createRoot } from "react-dom/client";
+import App from "./App";
 
-ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
-    <React.StrictMode>
-        <App />
-    </React.StrictMode>,
-);
+const container = document.getElementById("app");
+const root = createRoot(container)
+root.render(<App />);
